c0847fe63509e07be24116e08bbad1e1825de6ba
bahadzhalalov <bahadzhalalov@gmail.com>
2025-11-21 23:35:27 +0500

    remove debug route

--- internal/server/router.go
+++ internal/server/router.go
@@ -12,11 +12,6 @@ type Routers struct {
 func MainRouter(r Routers) *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte(`{"status":"ok"}`))
-	}).Methods("GET")
-
 	group := router.PathPrefix("/api").Subrouter()
 	group.PathPrefix("/teams").Handler(http.StripPrefix("/api/teams", r.teamRouter))
 
